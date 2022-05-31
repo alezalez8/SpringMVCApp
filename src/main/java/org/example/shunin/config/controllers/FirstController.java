f8f8a2e76082cbb49de5a02e781ca86a41734ee1
alezalez8 <5446943@ukr.net>
2022-05-31 19:31:31 +0300

    less21 is fully completed

--- src/main/java/org/example/shunin/config/controllers/FirstController.java
+++ src/main/java/org/example/shunin/config/controllers/FirstController.java
@@ -2,21 +2,30 @@ package org.example.shunin.config.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/helloy")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        model.addAttribute("message", "Hello " + name + "  " + surname);
+
         return "first/helloy";
 
     }
 
     @GetMapping("/goodbye")
     public String goodByePage() {
+
         return "first/goodbye";
     }
 
