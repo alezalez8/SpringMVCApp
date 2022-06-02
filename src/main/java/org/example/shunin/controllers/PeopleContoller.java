4e6a1239710e4bca449e0cb4cf56b649f05a44e9
alezalez8 <5446943@ukr.net>
2022-06-03 00:15:20 +0300

    less25 begin

--- src/main/java/org/example/shunin/controllers/PeopleContoller.java
+++ src/main/java/org/example/shunin/controllers/PeopleContoller.java
@@ -1,10 +1,12 @@
 package org.example.shunin.controllers;
 
+import jakarta.validation.Valid;
 import org.example.shunin.dao.PersonDAO;
 import org.example.shunin.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -44,7 +46,11 @@ public class PeopleContoller {
 
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "people/new";
+
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -57,8 +63,11 @@ public class PeopleContoller {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person,
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult,
                          @PathVariable("id") int id) {
+        if(bindingResult.hasErrors())
+            return "people/edit";
         personDAO.update(id, person);
         return "redirect:/people";
     }
