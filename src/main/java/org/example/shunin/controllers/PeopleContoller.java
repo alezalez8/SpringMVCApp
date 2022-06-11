cbf057e265eb90733293e23ce09681025deb48c9
alezalez8 <5446943@ukr.net>
2022-06-11 13:12:43 +0300

    lesson 43 end added language and opisanie

--- src/main/java/org/example/shunin/controllers/PeopleContoller.java
+++ src/main/java/org/example/shunin/controllers/PeopleContoller.java
@@ -10,6 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+
+/*
+* @author Aleks
+* */
 @Controller
 @RequestMapping("/people")
 public class PeopleContoller {
